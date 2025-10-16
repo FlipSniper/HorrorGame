d1fad0d8c7243cf291836060ca44a7a2ec2a6a1c
FlipSniper <m-navaneeth21@st-bernards.slough.sch.uk>
2025-10-16 07:19:55 +0100

    Fixed spawning bug

--- general_scripts/ice.gd
+++ general_scripts/ice.gd
@@ -5,7 +5,11 @@ extends Node3D
 func melt_ice():
 	$AnimationPlayer.play("melt")
 	await get_tree().create_timer(3).timeout
+
 	var card = key_card.instantiate()
 	card.global_position = global_position
+	card.global_position.y += 0.5
+	get_parent().add_child(card)
+
 	$ice/CollisionShape3D.disabled = true
 	$Cube_001.visible = false
