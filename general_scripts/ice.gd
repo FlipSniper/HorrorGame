a4a589040644500f9682eee72919a30ec559eb8c
FlipSniper <m-navaneeth21@st-bernards.slough.sch.uk>
2025-10-16 07:26:44 +0100

    FIXED KEY CARD SPAWN

--- general_scripts/ice.gd
+++ general_scripts/ice.gd
@@ -7,9 +7,11 @@ func melt_ice():
 	await get_tree().create_timer(3).timeout
 
 	var card = key_card.instantiate()
-	card.global_position = global_position
-	card.global_position.y += 0.5
+	card.global_position = position
 	get_parent().add_child(card)
+	card.scale.x = .2
+	card.scale.y = .2
+	card.scale.z = .2
 
 	$ice/CollisionShape3D.disabled = true
 	$Cube_001.visible = false
