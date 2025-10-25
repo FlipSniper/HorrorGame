5564832bb79358bf0556ee9b5ea14e7121c1cee0
FlipSniper <m-navaneeth21@st-bernards.slough.sch.uk>
2025-10-25 21:02:28 +0100

    assets + battery works in drawers now

--- general_scripts/spawns.gd
+++ general_scripts/spawns.gd
@@ -6,6 +6,7 @@ extends Node3D
 @export var matchsticks : PackedScene
 @export var boiler_wheel : PackedScene
 @export var magnets : PackedScene
+@export var batteries : PackedScene
 var pos
 
 func _ready() -> void:
@@ -29,3 +30,10 @@ func _ready() -> void:
 	magnet.position.y +=.035
 	print(pos)
 	static_spawns.remove_at(pos)
+	var battery = batteries.instantiate()
+	pos = rng.randi_range(0,rigid_spawns.size()-1)
+	add_child(battery)
+	battery.global_position = rigid_spawns[pos].global_position
+	battery.global_rotation = Vector3(deg_to_rad(90),0,0)
+	print(pos)
+	rigid_spawns.remove_at(pos)
